bc882927905c3a81b45f27977f0836e7634ba1a3
haythem <haythemmarouani@gmail.com>
2019-11-11 12:15:51 +0100

    add club model

---
+++ Models/Club.cs
@@ -0,0 +1,36 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace clubyApi.Models
+{
+    public class Club
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("Name")]
+        public string Name{ get;set;}
+        [BsonElement("Description")]
+        public string Description{ get;set;}
+        [BsonElement("Email")]
+        public string Email{ get ; set;}
+        [BsonElement("Password")]
+        public string Password{ get ; set;}
+        [BsonElement("Photo")]
+        public string Photo{ get;set;}
+        //[BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("Institut")]
+        public string Institut{ get ; set;}
+        //[BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("Domain")]
+        public string Domain{ get ; set;}
+         [BsonElement("CreationDate")]
+        public date CreationDate{ get ; set;}
+        
+
+
+
+
+
+    }
+}
