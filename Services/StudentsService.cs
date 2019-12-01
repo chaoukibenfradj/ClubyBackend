beeed91cd57b94e5ad8327549ad95c1b66b46661
yosraf <yosrafatnassi6@gmail.com>
2019-12-01 13:02:23 +0100

    modifying the student service

--- Services/StudentsService.cs
+++ Services/StudentsService.cs
@@ -4,46 +4,28 @@ using clubyApi.Models;
 using MongoDB.Driver;
 using clubyApi.Utils;
 using System;
+using clubyApi.Repositories;
+
 namespace clubyApi.Services
 {
-    public class StudentsService
+    public class StudentService :IStudentService
     {
-        private readonly IMongoCollection<Student> _students;
-        public StudentsService(IClubyDatabaseSettings settings){
-            var client = new MongoClient(settings.ConnectionString);
-            var database = client.GetDatabase(settings.DatabaseName);
-            _students = database.GetCollection<Student>(settings.StudentCollectionName); 
+        private readonly IStudentRepository _repo;
+        public StudentService(IStudentRepository repo){
+            _repo=repo;
 
         }
-        public string Create(Student student){
-            string result="email is already in use";
-            var hashPassword=new HashPassword();
-            long num = _students.Find<Student>(stud => stud.Email==student.Email).CountDocuments();
-            if(num==0){
-                result="account created with success";
-                student.Password=hashPassword.HashedPass(student.Password);
-                _students.InsertOne(student);
-            }
-            return result;
-        }
-        public Student Login(string email,string password){
-            
-            Student student = _students.Find<Student>(student => student.Email == email).FirstOrDefault();
-            if(student!=null){
-            
-                HashPassword hashPassword=new HashPassword();
-                if(student.Password != hashPassword.HashedPass(password)){
-                    student=null;
-                }
-                
-            }
-            return student;
-        } 
 
-        public Student GetProfile(string id){
-            Student student = _students.Find<Student>(student => student.Id == id).FirstOrDefault();
-            return student;
-        }
+        public Student AuthentificateStudent(string email, string password)=> _repo.AuthentificateStudent(email,password);
+        
+
+        public Student CreateStudent(Student student)=>_repo.CreateStudent(student);
+    }
+
+    public interface IStudentService
+    {
+        Student AuthentificateStudent(string email,string password);
+        Student CreateStudent(Student student)
 
     }
 }
