dd6a9f5678e3698456b4533ce0708a8fb4613f09
yosraf <yosrafatnassi6@gmail.com>
2019-12-04 15:41:57 +0100

    club

--- Controllers/ClubsController.cs
+++ Controllers/ClubsController.cs
@@ -12,14 +12,10 @@ namespace clubyApi.Controllers
     [ApiController]
     public class ClubsController :ControllerBase
     {   
-<<<<<<< HEAD
         private  readonly IClubService _service;
-        public ClubsController(IClubService service){
-=======
-        private readonly ClubService _service;
 
-        public ClubsController(ClubService service){
->>>>>>> ef87c74c77aeeb9280b9ff256469c37d252271ba
+
+        public ClubsController(IClubService service){
              _service=service;
         }
 
