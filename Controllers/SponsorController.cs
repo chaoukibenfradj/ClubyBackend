d39df35b22cd0174d83aaf58a98e68645d25cfd9
haythem <haythemmarouani@gmail.com>
2020-01-08 22:56:35 +0100

    fixing erreur lis

--- Controllers/SponsorController.cs
+++ Controllers/SponsorController.cs
@@ -1,5 +1,6 @@
 using clubyApi.Models;
 using clubyApi.Services;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
